1bb872df6d0df338800974433a834242a61c742e
Alex Glover <Alex@MacBook-Pro-3.local>
2017-04-17 17:10:09 -0400

    minor updates

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: { registrations: "registrations" }
   resources :users do
-    resources :decisions
+    resources :decisions, only: [:index]
     resources :avatars do
       post 'select', to: 'users#avatar_select'
     end
