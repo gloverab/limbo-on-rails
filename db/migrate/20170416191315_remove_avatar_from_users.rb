cf3a6d10c3899b8939ad1ed3083911f4b4e7052c
Alex Glover <Alex@MacBook-Pro-3.local>
2017-04-17 11:47:16 -0400

    Added profile card and SOUPED UP the main profile page style

---
+++ db/migrate/20170416191315_remove_avatar_from_users.rb
@@ -0,0 +1,6 @@
+class RemoveAvatarFromUsers < ActiveRecord::Migration[5.0]
+  def change
+    remove_column :users, :avatar
+    add_column :users, :avatar_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170415221621) do
+ActiveRecord::Schema.define(version: 20170416191315) do
 
   create_table "avatars", force: :cascade do |t|
     t.string "name"
@@ -51,7 +51,7 @@ ActiveRecord::Schema.define(version: 20170415221621) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "username"
-    t.string   "avatar"
+    t.integer  "avatar_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
