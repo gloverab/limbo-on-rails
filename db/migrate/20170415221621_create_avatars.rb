6bc6c98f1a695e120ef28f183ae7c7aa150ad984
Alex Glover <alexanderbglover@gmail.com>
2017-07-18 22:36:33 -0400

    fixed url type

--- db/migrate/20170415221621_create_avatars.rb
+++ db/migrate/20170415221621_create_avatars.rb
@@ -2,7 +2,7 @@ class CreateAvatars < ActiveRecord::Migration[5.0]
   def change
     create_table :avatars do |t|
       t.string :name
-      t.string :uploaded_avatar.url
+      t.string :uploaded_avatar
     end
   end
 end
